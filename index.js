be5a888cfc8b2e458f0e929799bda9c6bd98c57b
Thongchai <thongchai.wan@icloud.com>
2024-05-24 16:10:17 +0700

    try to use redirect

--- index.js
+++ index.js
@@ -19,21 +19,6 @@ app.get("/start", (req, res) => {
 });
 
 app.post("/start", (req, res) => {
-  res.render("board.ejs", { text: userPost, checker: postEditCheck });
-});
-
-app.post("/post", (req, res) => {
-  let userPosttext = req.body.UserPosts;
-  userPost.push(userPosttext);
-  console.log(userPost);
-  res.render("board.ejs", { text: userPost, checker: postEditCheck });
-});
-
-app.post("/edit", (req, res) => {
-  let editedPost = req.body.edit;
-  console.log(editedPost);
-  let postEditCheck = true;
-  console.log(userPost);
   res.render("board.ejs", {
     text: userPost,
     checker: postEditCheck,
@@ -41,22 +26,29 @@ app.post("/edit", (req, res) => {
   });
 });
 
+app.post("/post", (req, res) => {
+  let userPosttext = req.body.UserPosts;
+  userPost.push(userPosttext);
+  res.redirect("/start");
+});
+
+app.post("/edit", (req, res) => {
+  let editedPost = req.body.edit;
+  let postEditCheck = true;
+  res.redirect("/start");
+});
+
 app.post("/postedit", (req, res) => {
   let editedPost = req.body.EditPosts;
   let markpost = req.body.editpost;
-  console.log(editedPost);
-  console.log(markpost);
   userPost.splice(markpost, 1, editedPost);
-  console.log(userPost);
-  res.render("board.ejs", { text: userPost, checker: postEditCheck });
+  res.redirect("/start");
 });
 
 app.post("/delete", (req, res) => {
   let deletedPost = req.body.delete;
-  console.log(deletedPost);
   userPost.splice(deletedPost, 1);
-  console.log(userPost);
-  res.render("board.ejs", { text: userPost, checker: postEditCheck });
+  res.redirect("/start");
 });
 
 app.listen(port, () => {
